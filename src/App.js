d8bdcf94c24b82a555deafacd78fb3e61381bfb5
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2023-02-09 14:35:58 +0600

    feat: add task functionality add

--- src/App.js
+++ src/App.js
@@ -30,7 +30,14 @@ function App() {
 
   //Add Task
   //////////////////////
-  const addTask = () => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask('');
+    }
+  };
 
   //Delete Task
   //////////////////////
@@ -60,35 +67,66 @@ function App() {
       <br />
       <br />
 
+      {/* Update Task  */}
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-lg" />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success mr-20">Update</button>
+          <button className="btn btn-lg btn-warning">Cancel</button>
+        </div>
+      </div>
+      <br />
+
+      {/* Add Task  */}
+      <div className="row">
+        <div className="col">
+          <input
+            className="form-control form-control-lg"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success" onClick={addTask}>
+            Add Task
+          </button>
+        </div>
+      </div>
+      <br />
+
       {/* Display ToDos  */}
 
       {toDo && toDo.length ? '' : 'No Tasks...'}
 
       {toDo &&
-        toDo.map((task, index) => {
-          return (
-            <React.Fragment key={task.id}>
-              <div className="col taskBg">
-                <div className={task.status ? 'done' : ''}>
-                  <span className="taskNumber">{index + 1}</span>
-                  <span className="taskText">{task.title}</span>
-                </div>
+        toDo
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? 'done' : ''}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
 
-                <div className="iconsWrap">
-                  <span>
-                    <FontAwesomeIcon icon={faCircleCheck} />
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faPen} />
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon={faTrashCan} />
-                  </span>
+                  <div className="iconsWrap">
+                    <span title="Completed / Not Completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </React.Fragment>
-          );
-        })}
+              </React.Fragment>
+            );
+          })}
     </div>
   );
 }
